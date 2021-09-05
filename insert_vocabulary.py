e7b4953b5a00053a8da1ae53c502fa4956bf47fd
Hetorus NL <timkleinnijenhuis@hotmail.com>
2021-09-05 21:52:04 +0200

    updated the inject_vocabulary script to handle the added courses and user configurable fields

--- insert_vocabulary.py
+++ insert_vocabulary.py
@@ -1,14 +1,17 @@
 import json
 
 
-def get_user_input():
+def get_user_input(course):
     # get chapter and next id from user
     with open("dictionary.json") as f:
         dict = json.load(f)
-        max_chapter = int(
-            max(dict["vocabulary"], key=lambda a: int(a["chapter"]))["chapter"]
-        )
-        max_id = int(max(dict["vocabulary"], key=lambda a: int(a["id"]))["id"])
+        max_chapter = 0
+        max_id = 0
+        # ugly last chapter/id fetching
+        last_item = dict["vocabulary"][-1]
+        if last_item["course"] == f"{course}":
+            max_chapter = int(last_item["chapter"][5:])
+            max_id = int(last_item["id"][5:])
     chapter = input(f"chapter ({max_chapter + 1}): ")
     next_id = input(f"next id ({max_id + 1}): ")
 
@@ -24,21 +27,23 @@ def get_user_input():
     return {"chapter": chapter, "next_id": next_id}
 
 
-def parse_vocab(vocab, id, chapter):
+def parse_vocab(vocab, id, chapter, course, fields_to_enter):
     # verify the passed vocab
     parsed_vocab = vocab.split(";")
-    if len(parsed_vocab) != 2:
+    fields_to_enter = fields_to_enter.split(";")
+    if len(parsed_vocab) != len(fields_to_enter):
         return False
 
     # vocab is correct, add to the dictionary
     with open("dictionary.json") as f:
         dict = json.load(f)
     obj = {
-        "id": str(id),
-        "chapter": str(chapter),
-        "romaji": parsed_vocab[0].strip(),
-        "dutch": parsed_vocab[1].strip(),
+        "id": f"{course}-{str(id)}",
+        "course": course,
+        "chapter": f"{course}-{str(chapter)}",
     }
+    for i in range(len(parsed_vocab)):
+        obj[fields_to_enter[i]] = parsed_vocab[i]
     dict["vocabulary"].append(obj)
     with open("dictionary.json", "w") as f:
         json.dump(dict, f, indent=2)
@@ -47,9 +52,17 @@ def parse_vocab(vocab, id, chapter):
 
 
 def main():
-    user_input = get_user_input()
+    course = "jem2"
+    override_course = input(f"course ({course}): ")
+    if override_course:
+        course = override_course
+    user_input = get_user_input(course)
     chapter = user_input["chapter"]
     next_id = user_input["next_id"]
+    fields_to_enter = input(
+        "enter fields to be entered separated by ';' "
+        "(e.g. romaji;dutch;hiragana): "
+    )
     with open("dictionary.json") as f:
         dict = json.load(f)
     chapter_text = list(
@@ -59,11 +72,11 @@ def main():
     print(f"next chapter: {chapter} ({chapter_text})")
     print(f"next id: {next_id}")
     print()
-    print("add words by entering 'romaji;dutch' on every new line:")
+    print(f"add words by entering '{fields_to_enter}' on every new line:")
 
     next_vocab = input(f"next id = {next_id}: ")
     while next_vocab:
-        if parse_vocab(next_vocab, next_id, chapter):
+        if parse_vocab(next_vocab, next_id, chapter, course, fields_to_enter):
             next_id += 1
         else:
             print("invalid input, retry...")
