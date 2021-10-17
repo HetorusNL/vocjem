5b03b0db5b5c9894a9a66dcf543e8c2129de6bd0
Hetorus NL <hetorusnl@DebianVM>
2021-10-17 21:25:09 +0200

    updated insert_kana script to handle vocabulair ids with embedded course ids

--- insert_kana.py
+++ insert_kana.py
@@ -1,14 +1,15 @@
 import json
 
 
-def get_user_input(kana):
+def get_user_input(course, kana):
     # get next id from user
     with open("dictionary.json") as f:
         _dict = json.load(f)
-        max_id = 0
+        max_id = f"{course}-0"
+        # ugly last id fetching
         for obj in _dict["vocabulary"]:
             max_id = obj["id"] if kana in obj else max_id
-        max_id = int(max_id["id"] if type(max_id) == dict else max_id)
+        max_id = int(max_id[5:])
     next_id = input(f"next id ({max_id + 1}): ")
 
     # verify next_id
@@ -20,11 +21,11 @@ def get_user_input(kana):
     return {"next_id": next_id}
 
 
-def parse_vocab(vocab, id, kana):
+def parse_vocab(course, vocab, id, kana):
     with open("dictionary.json") as f:
         _dict = json.load(f)
     for obj in _dict["vocabulary"]:
-        if str(id) == obj["id"]:
+        if f"{course}-{id}" == obj["id"]:
             obj[kana] = vocab
     with open("dictionary.json", "w") as f:
         json.dump(_dict, f, indent=2)
@@ -32,31 +33,37 @@ def parse_vocab(vocab, id, kana):
     return True
 
 
-def get_next_id(next_id):
+def get_next_id(course, next_id):
     with open("dictionary.json") as f:
         _dict = json.load(f)
         res = list(
-            filter(lambda a: a["id"] == str(next_id), _dict["vocabulary"])
+            filter(lambda a: a["id"] == f"{course}-{next_id}", _dict["vocabulary"])
         )
+        hiragana = res[0]["hiragana"] if res else ""
         romaji = res[0]["romaji"] if res else ""
-    return f"next id = {next_id} ({romaji}): "
+        info = "/".join([hiragana, romaji])
+    return f"next id = {next_id} ({info}): "
 
 
 def main():
+    course = "jem1"
+    override_course = input(f"course ({course}): ")
+    if override_course:
+        course = override_course
     kana = input("which kana? [hiragana/kanji]: ")
-    user_input = get_user_input(kana)
+    user_input = get_user_input(course, kana)
     next_id = user_input["next_id"]
     print(f"next id: {next_id}")
     print()
     print(f"add words by entering '{kana}' on every new line:")
 
-    next_vocab = input(get_next_id(next_id))
+    next_vocab = input(get_next_id(course, next_id))
     while next_vocab:
-        if parse_vocab(next_vocab, next_id, kana):
+        if parse_vocab(course, next_vocab, next_id, kana):
             next_id += 1
         else:
             print("invalid input, retry...")
-        next_vocab = input(get_next_id(next_id))
+        next_vocab = input(get_next_id(course, next_id))
 
 
 if __name__ == "__main__":
