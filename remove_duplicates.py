1fc50a2695a5ca3bcaac77b04692bf5648d4525a
Hetorus NL <timkleinnijenhuis@hotmail.com>
2023-01-04 15:32:36 +0100

    removed additional duplicates from jem-old-1

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -46,6 +46,7 @@ def main():
         if any(word_equal(word, rest_word) for rest_word in rest_words):
             print(f"removing duplicate word: {word['id']}")
             vocabulary.remove(word)
+            write_dictionary(vocabulary)
     from_words: list[dict[str, str]] = [entry for entry in vocabulary if entry["course"] == from_course]
     print(f"pruned {len_before_pruning-len(from_words)} words")
 
@@ -67,6 +68,8 @@ def main():
                 equal_words += 1
         from_words: list[dict[str, str]] = [entry for entry in vocabulary if entry["course"] == from_course]
         print(f"words equal in {key}: {equal_words}")
+
+    print(f"words left: {len(from_words)}")
 
     write_dictionary(vocabulary)
 
